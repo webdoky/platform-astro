6557792763b23dfee4f8f56a19f38b62d0e09352
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2024-02-09 15:39:40 +0200

    fix TOC

--- src/components/toc.ts
+++ src/components/toc.ts
@@ -1,17 +1,16 @@
 const tocHeading = document.querySelector('#toc-heading');
-if (!tocHeading) {
-  throw new Error('No TOC heading found');
+if (tocHeading) {
+  const icon = tocHeading.querySelector('svg');
+  if (!icon) {
+    throw new Error('No TOC icon found');
+  }
+  tocHeading.role = 'button';
+  const collapsible = tocHeading.nextElementSibling;
+  if (!collapsible) {
+    throw new Error('No TOC found');
+  }
+  tocHeading.addEventListener('click', () => {
+    icon.classList.toggle('rotate-180');
+    collapsible.classList.toggle('max-h-0');
+  });
 }
-const icon = tocHeading.querySelector('svg');
-if (!icon) {
-  throw new Error('No TOC icon found');
-}
-tocHeading.role = 'button';
-const collapsible = tocHeading.nextElementSibling;
-if (!collapsible) {
-  throw new Error('No TOC found');
-}
-tocHeading.addEventListener('click', () => {
-  icon.classList.toggle('rotate-180');
-  collapsible.classList.toggle('max-h-0');
-});
